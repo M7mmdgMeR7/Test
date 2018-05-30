1b9b4cff8e7bdb7924eb1171babd568ba1a98c7c
M7mmdgMeR7 <Mohalaamer@icloud.com>
2018-05-30 19:16:39 +0300

    Update Main.php

--- Test/src/Main.php
+++ Test/src/Main.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace Test;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase{
+  
+  public function onEnable(){
+     $this->getLogger()->info(TextFormat::GREEN."Test Enabled");
+  }
+}
